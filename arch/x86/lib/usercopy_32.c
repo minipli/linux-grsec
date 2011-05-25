34ac2405adea2b74f28ad886c40919f947844afe
PaX Team <pageexec@freemail.hu>
2011-05-26 01:13:09 +0200

    [pax] Updated to patch 2.6.36.2-test10
    Updated to pax-linux-2.6.36.2-test10.patch

--- arch/x86/lib/usercopy_32.c
+++ arch/x86/lib/usercopy_32.c
@@ -964,9 +964,8 @@ void copy_to_user_overflow(void)
 EXPORT_SYMBOL(copy_to_user_overflow);
 
 #ifdef CONFIG_PAX_MEMORY_UDEREF
-void set_fs(mm_segment_t x)
+void __set_fs(mm_segment_t x)
 {
-	current_thread_info()->addr_limit = x;
 	switch (x.seg) {
 	case 0:
 		loadsegment(gs, 0);
@@ -982,6 +981,11 @@ void set_fs(mm_segment_t x)
 	}
 	return;
 }
-#endif
 
+void set_fs(mm_segment_t x)
+{
+	current_thread_info()->addr_limit = x;
+	__set_fs(x);
+}
 EXPORT_SYMBOL(set_fs);
+#endif
