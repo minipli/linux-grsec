33675e0da9b628b4aaa90d459874af66376b8130
PaX Team <pageexec@freemail.hu>
2011-05-25 20:53:22 +0200

    [pax] Updated to patch 2.6.24.4-test40
    Updated to pax-linux-2.6.24.4-test40.patch

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1717,7 +1717,7 @@ extern void __cleanup_signal(struct signal_struct *);
 extern void __cleanup_sighand(struct sighand_struct *);
 extern void exit_itimers(struct signal_struct *);
 
-extern NORET_TYPE void do_group_exit(int);
+extern NORET_TYPE void do_group_exit(int) ATTRIB_NORET;
 
 extern void daemonize(const char *, ...);
 extern int allow_signal(int);
