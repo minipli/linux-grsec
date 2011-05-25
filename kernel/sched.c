23d50589ef43ea83667a06cbe45dd94fd10d4e44
PaX Team <pageexec@freemail.hu>
2011-05-26 00:15:11 +0200

    [pax] Applied patch 2.6.32.6-test9
    This is pax-linux-2.6.32.6-test9.patch

--- kernel/sched.c
+++ kernel/sched.c
@@ -7421,7 +7421,7 @@ static struct ctl_table sd_ctl_dir[] = {
 		.procname	= "sched_domain",
 		.mode		= 0555,
 	},
-	{0, },
+	{ 0, NULL, NULL, 0, 0, NULL, NULL, NULL, NULL, NULL, NULL }
 };
 
 static struct ctl_table sd_ctl_root[] = {
@@ -7431,7 +7431,7 @@ static struct ctl_table sd_ctl_root[] = {
 		.mode		= 0555,
 		.child		= sd_ctl_dir,
 	},
-	{0, },
+	{ 0, NULL, NULL, 0, 0, NULL, NULL, NULL, NULL, NULL, NULL }
 };
 
 static struct ctl_table *sd_alloc_ctl_entry(int n)
