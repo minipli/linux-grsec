deb5bf6642c536f5dd566ca2a6ddb1c799e5890f
PaX Team <pageexec@freemail.hu>
2011-05-25 20:58:57 +0200

    [pax] Updated to patch 2.6.24.7-test50
    Updated to pax-linux-2.6.24.7-test50.patch

--- include/linux/init_task.h
+++ include/linux/init_task.h
@@ -121,7 +121,7 @@ extern struct group_info init_groups;
 #define INIT_TASK(tsk)	\
 {									\
 	.state		= 0,						\
-	.stack		= &init_thread_union,				\
+	.stack		= &init_thread_info,				\
 	.usage		= ATOMIC_INIT(2),				\
 	.flags		= 0,						\
 	.lock_depth	= -1,						\
