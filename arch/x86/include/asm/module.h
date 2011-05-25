a433c7647549b9b44da40540b21c1c0fb884260d
PaX Team <pageexec@freemail.hu>
2011-05-26 00:28:20 +0200

    [pax] Updated to patch 2.6.33.2-test11
    Updated to pax-linux-2.6.33.2-test11.patch

--- arch/x86/include/asm/module.h
+++ arch/x86/include/asm/module.h
@@ -59,6 +59,12 @@
 #error unknown processor family
 #endif
 
+#ifdef CONFIG_PAX_MEMORY_UDEREF
+#define MODULE_PAX_UDEREF "UDEREF "
+#else
+#define MODULE_PAX_UDEREF ""
+#endif
+
 #ifdef CONFIG_X86_32
 # ifdef CONFIG_4KSTACKS
 #  define MODULE_STACKSIZE "4KSTACKS "
@@ -70,12 +76,9 @@
 # else
 #  define MODULE_PAX_KERNEXEC ""
 # endif
-# ifdef CONFIG_PAX_MEMORY_UDEREF
-#  define MODULE_PAX_UDEREF "UDEREF "
-# else
-#  define MODULE_PAX_UDEREF ""
-# endif
 # define MODULE_ARCH_VERMAGIC MODULE_PROC_FAMILY MODULE_STACKSIZE MODULE_PAX_KERNEXEC MODULE_PAX_UDEREF
+#else
+# define MODULE_ARCH_VERMAGIC MODULE_PAX_UDEREF
 #endif
 
 #endif /* _ASM_X86_MODULE_H */
