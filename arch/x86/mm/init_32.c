9cec05de2dfc9f9bbc3cc20a28304351acd57c96
PaX Team <pageexec@freemail.hu>
2011-05-25 21:40:51 +0200

    [pax] Updated to patch 2.6.26.3-test15
    Updated to pax-linux-2.6.26.3-test15.patch

--- arch/x86/mm/init_32.c
+++ arch/x86/mm/init_32.c
@@ -237,7 +237,7 @@ int devmem_is_allowed(unsigned long pagenr)
 {
 	if (!pagenr)
 		return 1;
-	if (160 <= pagenr && pagenr <= 256)
+	if (160 <= pagenr && pagenr < 256)
 		return 1;
 	if (!page_is_ram(pagenr))
 		return 1;
