ef7d84d3c093dd37caf7e543ef8e950298f6d195
PaX Team <pageexec@freemail.hu>
2011-05-26 10:56:29 +0200

    [pax] Updated to patch 2.6.33.5-test27
    Updated to pax-linux-2.6.33.5-test27.patch

--- arch/x86/mm/setup_nx.c
+++ arch/x86/mm/setup_nx.c
@@ -5,9 +5,10 @@
 #include <asm/pgtable.h>
 #include <asm/proto.h>
 
+#if defined(CONFIG_X86_64) || defined(CONFIG_X86_PAE)
 static int disable_nx __cpuinitdata;
 
-#if defined(CONFIG_X86_PAE) && !defined(CONFIG_PAX_PAGEEXEC)
+#ifndef CONFIG_PAX_PAGEEXEC
 /*
  * noexec = on|off
  *
@@ -31,11 +32,15 @@ static int __init noexec_setup(char *str)
 early_param("noexec", noexec_setup);
 #endif
 
+#endif
+
 void __cpuinit x86_configure_nx(void)
 {
+#if defined(CONFIG_X86_64) || defined(CONFIG_X86_PAE)
 	if (cpu_has_nx && !disable_nx)
 		__supported_pte_mask |= _PAGE_NX;
 	else
+#endif
 		__supported_pte_mask &= ~_PAGE_NX;
 }
 
