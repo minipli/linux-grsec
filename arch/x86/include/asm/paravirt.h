9529f141e097accc362d7f450241c912021fefe1
PaX Team <pageexec@freemail.hu>
2011-08-24 06:40:37 +0200

    [pax] Updated to patch 3.0.3-test4
    Updated to pax-linux-3.0.3-test4.patch

--- arch/x86/include/asm/paravirt.h
+++ arch/x86/include/asm/paravirt.h
@@ -660,7 +660,14 @@ static inline void set_pgd(pgd_t *pgdp, pgd_t pgd)
 
 static inline void set_pgd_batched(pgd_t *pgdp, pgd_t pgd)
 {
-	set_pgd(pgdp, pgd);
+	pgdval_t val = native_pgd_val(pgd);
+
+	if (sizeof(pgdval_t) > sizeof(long))
+		PVOP_VCALL3(pv_mmu_ops.set_pgd_batched, pgdp,
+			    val, (u64)val >> 32);
+	else
+		PVOP_VCALL2(pv_mmu_ops.set_pgd_batched, pgdp,
+			    val);
 }
 
 static inline void pgd_clear(pgd_t *pgdp)
