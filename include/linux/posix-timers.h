8afe6fe7446929fd339c9da7401e5c66960988a7
PaX Team <pageexec@freemail.hu>
2012-02-26 10:00:50 +0100

    [pax] Updated to patch 2.6.32.57-test140
    Updated to pax-linux-2.6.32.57-test140.patch

--- include/linux/posix-timers.h
+++ include/linux/posix-timers.h
@@ -67,7 +67,7 @@ struct k_itimer {
 };
 
 struct k_clock {
-	const int res;		/* in nanoseconds */
+	int res;		/* in nanoseconds */
 	int (*clock_getres) (const clockid_t which_clock, struct timespec *tp);
 	int (*clock_set) (const clockid_t which_clock, struct timespec * tp);
 	int (*clock_get) (const clockid_t which_clock, struct timespec * tp);
@@ -82,7 +82,8 @@ struct k_clock {
 #define TIMER_RETRY 1
 	void (*timer_get) (struct k_itimer * timr,
 			   struct itimerspec * cur_setting);
-};
+} __do_const;
+typedef struct k_clock __no_const k_clock_no_const;
 
 void register_posix_clock(const clockid_t clock_id, struct k_clock *new_clock);
 
