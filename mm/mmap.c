65ffbe0d5407dc7cc7fda81d3c67f172df24fd7c
PaX Team <pageexec@freemail.hu>
2012-03-09 23:59:36 +0100

    [pax] Updated to patch 3.2.9-test21
    Updated to pax-linux-3.2.9-test21.patch

--- mm/mmap.c
+++ mm/mmap.c
@@ -1827,7 +1827,16 @@ find_vma_prev(struct mm_struct *mm, unsigned long addr,
 	struct vm_area_struct *vma;
 
 	vma = find_vma(mm, addr);
-	*pprev = vma ? vma->vm_prev : NULL;
+	if (vma) {
+		*pprev = vma->vm_prev;
+	} else {
+		struct rb_node *rb_node = mm->mm_rb.rb_node;
+		*pprev = NULL;
+		while (rb_node) {
+			*pprev = rb_entry(rb_node, struct vm_area_struct, vm_rb);
+			rb_node = rb_node->rb_right;
+		}
+	}
 	return vma;
 }
 
