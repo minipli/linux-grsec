b044536ed05437a8b4a883dcae537c597f90ef6d
PaX Team <pageexec@freemail.hu>
2012-09-23 12:18:20 +0200

    [pax] Updated to patch 3.2.30-test77
    Updated to pax-linux-3.2.30-test77.patch
    
    PaX Team says:
      - made the constify plugin more robust based on results from the gcc test suite
      - fixed softmode/EMUTRAMP regression (executables produced by the patched binutils would not run)
      - fixed kernel stack disclosure in sys_newuname affecting linux 3.x

--- kernel/sys.c
+++ kernel/sys.c
@@ -1170,13 +1170,13 @@ DECLARE_RWSEM(uts_sem);
  * Work around broken programs that cannot handle "Linux 3.0".
  * Instead we map 3.x to 2.6.40+x, so e.g. 3.0 would be 2.6.40
  */
-static int override_release(char __user *release, int len)
+static int override_release(char __user *release, size_t len)
 {
 	int ret = 0;
-	char buf[65];
 
 	if (current->personality & UNAME26) {
-		char *rest = UTS_RELEASE;
+		char buf[65] = { 0 };
+		const char *rest = UTS_RELEASE;
 		int ndots = 0;
 		unsigned v;
 
@@ -1188,7 +1188,10 @@ static int override_release(char __user *release, int len)
 			rest++;
 		}
 		v = ((LINUX_VERSION_CODE >> 8) & 0xff) + 40;
+		if (sizeof buf < len)
+			len = sizeof buf;
 		snprintf(buf, len, "2.6.%u%s", v, rest);
+		buf[len - 1] = 0;
 		ret = copy_to_user(release, buf, len);
 	}
 	return ret;
