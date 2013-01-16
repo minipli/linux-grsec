c8858faa0811a7cb1ea4b5371614c80dfbf052b3
PaX Team <pageexec@freemail.hu>
2013-01-16 21:15:49 +0100

    [pax] Applied patch 3.7.2-test5
    This is pax-linux-3.7.2-test5.patch
    
    PaX Team says:
      - forward port to 3.7.2

--- arch/x86/kernel/head32.c
+++ arch/x86/kernel/head32.c
@@ -18,6 +18,7 @@
 #include <asm/io_apic.h>
 #include <asm/bios_ebda.h>
 #include <asm/tlbflush.h>
+#include <asm/boot.h>
 
 static void __init i386_default_early_setup(void)
 {
@@ -30,8 +31,7 @@ static void __init i386_default_early_setup(void)
 
 void __init i386_start_kernel(void)
 {
-	memblock_reserve(__pa_symbol(&_text),
-			 __pa_symbol(&__bss_stop) - __pa_symbol(&_text));
+	memblock_reserve(LOAD_PHYSICAL_ADDR, __pa_symbol(&__bss_stop) - LOAD_PHYSICAL_ADDR);
 
 #ifdef CONFIG_BLK_DEV_INITRD
 	/* Reserve INITRD */
