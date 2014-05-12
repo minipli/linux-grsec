cfc091f00b79c005562ee6032924f016b9f3cdf8
PaX Team <pageexec@freemail.hu>
2014-05-12 17:44:02 +0200

    [pax] Applied patch 3.14.3-test7
    This is pax-linux-3.14.3-test7.patch
    
    PaX Team says:
      - forward port to 3.14.3
      - removed the no longer necessary mmap_sem locking from binfmt_flat, reported by Lionel Debroux
      - fixed resume on i386/KERNEXEC, reported by vincent (https://forums.grsecurity.net/viewtopic.php?f=3&t=3176)
      - Emese refactored the size overflow plugin and also fixed several integer constant related false positives
        - https://forums.grsecurity.net/viewtopic.php?f=3&t=3940
        - https://forums.grsecurity.net/viewtopic.php?f=3&t=3942
        - https://forums.grsecurity.net/viewtopic.php?f=3&t=3943
        - https://forums.grsecurity.net/viewtopic.php?f=3&t=3949
        - https://forums.grsecurity.net/viewtopic.php?f=3&t=3950

--- include/linux/init_task.h
+++ include/linux/init_task.h
@@ -156,6 +156,12 @@ extern struct task_group root_task_group;
 
 #define INIT_TASK_COMM "swapper"
 
+#ifdef CONFIG_X86
+#define INIT_TASK_THREAD_INFO .tinfo = INIT_THREAD_INFO,
+#else
+#define INIT_TASK_THREAD_INFO
+#endif
+
 #ifdef CONFIG_RT_MUTEXES
 # define INIT_RT_MUTEXES(tsk)						\
 	.pi_waiters = RB_ROOT,						\
@@ -203,6 +209,7 @@ extern struct task_group root_task_group;
 	RCU_POINTER_INITIALIZER(cred, &init_cred),			\
 	.comm		= INIT_TASK_COMM,				\
 	.thread		= INIT_THREAD,					\
+	INIT_TASK_THREAD_INFO						\
 	.fs		= &init_fs,					\
 	.files		= &init_files,					\
 	.signal		= &init_signals,				\
